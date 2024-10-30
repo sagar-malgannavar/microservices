4616c76aa048d2bc4c0bf3c00a59353ab4ffb857
Sagar Malgannavar <smalgannavar@gmail.com>
2024-10-30 18:44:17 +0530

    Files upload

---
+++ section_12/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -0,0 +1,42 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "accounts")
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
+}
